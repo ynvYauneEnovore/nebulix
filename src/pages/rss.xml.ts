a6d64e71fe87e99a034b488ff39451478231b0c1
ynvYauneEnovore <yovanuxf@gmail.com>
2023-09-20 15:21:28 -0400

    tailwindlksd

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -9,9 +9,9 @@ export async function get() {
 	posts = posts.slice(0, 3)
 
 	return rss({
-		title: 'VisVrs: An Astro Theme for Visual Storytelling',
-		description: 'VisVrs is an Astro theme specifically designed for photo and text blogging, making it ideal for blog authors and content creators.',
-		site: 'https://visvrs.vercel.app',
+		title: 'yovanenovore: Narración Visual sobre mi yo',
+		description: 'VisVrs es un tema Astro diseñado específicamente para blogs de fotos y texto, lo que lo hace ideal para autores de blogs y creadores de contenido.',
+		site: 'https://yovanenovore.tech',
 		items: posts.map((post) => ({
 			title: post.data.title,
 			pubDate: post.data.pubDate,
